047c1f10a6ba1831cee7e6601d7a305c7b90afaf
stealcompass <venya.e@gmail.com>
2022-10-11 13:03:50 +0300

    First homework: simple app with counter

--- exercise/ViewController.swift
+++ exercise/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var cnt: Int = 0
+    
+    @IBOutlet weak var pushInfo: UILabel!
+    
+    @IBOutlet weak var pushButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //pushButton.backgroundColor = .gray
+        //pushButton.titleLabel?.textColor = .green
+        //pushButton.titleLabel?.text = "Жми"
+
+        pushInfo.textAlignment = .center
+        pushInfo.text = "Значение счётчика: \(cnt)"
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func buttonAction(_ sender: Any) {
+        cnt = cnt + 1
+        pushInfo.text = "Значение счётчика: \(cnt)"
+    }
+    
 }
 
