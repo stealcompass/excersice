4790a2b53e3074f70e86748f7781c54f7f954e2d
stealcompass <venya.e@gmail.com>
2022-10-11 11:32:04 +0300

    Initial Commit

---
+++ exercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  exercise
+//
+//  Created by Veniamin on 11.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
